41148114d3e613b9352ea103160aa8edb4641961
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-12-06 16:30:05 +0300

    prod

--- src/middlewares/base.py
+++ src/middlewares/base.py
@@ -18,26 +18,32 @@ class ProtectMiddleware(BaseMiddleware):
             data: Dict[str, Any],
     ) -> Any:
         user = await user_service.add_user(event.from_user.id, event.from_user.username)
-        user = await user_service.get_user(event.from_user.id)
-        if user.role_id == 4:
-            if data["raw_state"] == "PasswordForm:get_password":
-                auth = await password_service.login(event.text, event.from_user.id)
-                if auth:
-                    if auth.role_id == 1:
-                        await event.answer(
-                            "Вы зашли как <b>супер-админ</b>."
-                        )
-                    if auth.role_id == 2:
-                        await event.answer(
-                            "Вы зашли как <b>админ</b>.",
-                            reply_markup=main_keyboard()
-                        )
+        print(f"user = {user}")
+        try:
+            if user.role_id == 4:
+                if data["raw_state"] == "PasswordForm:get_password":
+                    auth = await password_service.login(event.text, event.from_user.id)
+                    if auth:
+                        if auth.role_id == 1:
+                            await event.answer(
+                                "Вы зашли как <b>супер-админ</b>."
+                            )
+                        if auth.role_id == 2:
+                            await event.answer(
+                                "Вы зашли как <b>админ</b>.",
+                                reply_markup=main_keyboard()
+                            )
+                    else:
+                        await event.answer("Неверный пароль!")
                 else:
-                    await event.answer("Неверный пароль!")
+                    await event.answer(
+                        "Вы не имеете доступа к этому боту.",
+                        reply_markup=get_password_keyboard()
+                    )
             else:
-                await event.answer(
-                    "Вы не имеете доступа к этому боту.",
-                    reply_markup=get_password_keyboard()
-                )
-        else:
-            await handler(event, data)
+                await handler(event, data)
+        except AttributeError:
+            await event.answer(
+                "Вы не имеете доступа к этому боту.",
+                reply_markup=get_password_keyboard()
+            )
